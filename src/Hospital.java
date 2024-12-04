e1667e6b5ea7891a34a7a3570a7409c76093fb39
kavitha0507 <kavithamynala@gmail.com>
2024-12-03 22:19:55 -0600

    Initial commit

---
+++ src/Hospital.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Hospital {
+    public static void main(String[] args) {
+        Surgeon surgeon = new Surgeon();
+        surgeon.Doctor_Details();
+        surgeon.SurgeonDetails();
+
+
+    }
+}
---
+++ src/Surgeon.java
@@ -0,0 +1,11 @@
+public class Surgeon extends Doctor{
+    void SurgeonDetails(){
+        System.out.println("Surgeon details:");
+        System.out.println(Department = "Pediatrician");
+
+
+    }
+
+    }
+
+
